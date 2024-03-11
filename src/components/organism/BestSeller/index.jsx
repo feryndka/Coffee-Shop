8e0c3971ef82cbd2ce0aafe74a4f09d52376d0b1
feryndka <feryandika151@gmail.com>
2024-03-11 17:20:20 +0700

    style:finish page best seller

--- src/components/organism/BestSeller/index.jsx
+++ src/components/organism/BestSeller/index.jsx
@@ -1,5 +1,8 @@
+'use client'
 import Card from "@/components/molecules/Card-3d";
 import React from "react";
+
+import Slider from "../Slider";
 
 const BestSeller = () => {
   const content = [
@@ -23,16 +26,45 @@ const BestSeller = () => {
       subtitle: "Tumblers",
       image: "/assets/imagesCoffee/menu/merchandise-1.avif",
     },
+    {
+      title: "Drinks",
+      subtitle: "Cold Drinks",
+      image: "/assets/imagesCoffee/menu/drink-8.avif",
+    },
+    {
+      title: "Food",
+      subtitle: "Burger",
+      image: "/assets/imagesCoffee/menu/food-1.avif",
+    },
+    {
+      title: "Drinks",
+      subtitle: "Iced Teas",
+      image: "/assets/imagesCoffee/menu/drink-7.avif",
+    },
+    {
+      title: "Merchandise",
+      subtitle: "Cup",
+      image: "/assets/imagesCoffee/menu/merchandise-2.jpeg",
+    },
   ];
+
   return (
-    <div>
-      <div className="md:text-5xl text-3xl font-bold text-center p-10">
+    <div className="md:flex md:flex-col h-[90vh] items-center justify-center">
+      <div className="md:text-5xl text-3xl font-bold">
         Best Seller In Our Store
       </div>
-      <div className="md:flex md:gap-10 justify-center">
-        {content.map((item, index) => (
-          <Card key={index} title={item.title} subtitle={item.subtitle} image={item.image} />
-        ))}
+      <div>
+        <Slider>
+          {content.map((item, index) => (
+            <Slider.Content key={index}>
+              <Card
+                title={item.title}
+                subtitle={item.subtitle}
+                image={item.image}
+              />
+            </Slider.Content>
+          ))}
+        </Slider>
       </div>
     </div>
   );
